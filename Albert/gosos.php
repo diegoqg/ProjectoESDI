62bccfdf4ad727d7cfa4fc0019ba80900708395a
aavell97 <albert.avellaneda12@gmail.com>
2018-01-22 16:39:38 +0100

    Add files via upload

---
+++ Albert/gosos.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+	
+</head>
+  <body>
+
+    <!-- Navigation -->
+
+	
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.html">Home</a>
+        </li>
+      </ol>
+
+      <!-- Portfolio Item Row -->
+      <div class="row">
+
+        <div class="col-md-8">
+          <img class="img-fluid" src="https://cdnb.20m.es/animalesenadopcion/files/2010/08/Perrera-Municipal-de-Priego-de-C%C3%B3rdoba-1.jpg" alt="">
+        </div>
+
+        <div class="col-md-4">
+          <h3 class="my-3">Perrera Vazquez</h3>
+          <p>Pos esta perrera es la ostia socios</p>
+          <h3 class="my-3">Detalles perrera</h3>
+          <ul>
+            <li>Ubicacion: </li>
+            <li>Contacto:</li>
+            <li>hehexd</li>
+     
+          </ul>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+      <!-- Related Projects Row -->
+      <h3 class="my-4">Perros</h3>
+	  <?php
+		echo "<div class='row'>";
+		include("proba.php");
+		echo "</div>";
+		?>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+	<script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Footer -->
+  </body>
+
+</html>
