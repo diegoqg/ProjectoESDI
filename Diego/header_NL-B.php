a0c6d51c2bce71a717c76c9dbb48654a3b04af73
diegoqg <diegoqueraltgalan@gmail.com>
2018-02-16 19:49:41 +0100

    a
    a

--- Diego/header_NL-B.php
+++ Diego/header_NL-B.php
@@ -26,7 +26,7 @@
       <nav class="navbar navbar-expand-lg navbar-light bg-faded menu-relative fixed-top" style="background-color: white">
         <div class="container">
           <a class="navbar-brand" href="index.php">
-            <img src="images/logo.png" width="150px" height="50px" alt="">
+            <img src="images/logo.png" width="auto" height="50px" alt="">
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
