e544392e203a7de0adf9a6aa4531fff873848401
Franklinortuno <franklin.ortu@gmail.com>
2018-02-18 21:30:10 +0100

    xd

--- Diego/animal.php
+++ Diego/animal.php
@@ -1,10 +1,10 @@
-	
+
 		<?php
-			function modalAnimal($id){
-				$servername = "localhost";
+			$servername = "localhost";
 			$username = "root";
 			$password = "1234";
 			$dbname = "pethome";
+			$id = $_GET['id'];
 			$conn = new mysqli($servername, $username, $password, $dbname);
 								
 			if ($conn->connect_error) {
@@ -40,7 +40,7 @@
 			$imagen2 = "imagen2";
 			if ($num_filas > 0) {
 			while ($line = mysqli_fetch_array($result) ) {
-
+			  
 				echo "
 				    <div class=\"contenedor\">
 					<a href=\"#openmodal\" class=\"open\">Abrir Ventana</a>
@@ -49,55 +49,16 @@
 					<a href=\"#close\" class=\"close\"> X </a>
 					   	 <section class='modal-item uno'>
 						  	<div class=\"titulo\">
-						   		<h2>".$line[$nombreMascota]."</h2>";
-
-			    echo "
-			    <div class=\"contenedor\">
-				<!--<a href=\"#openmodal\" class=\"open\">Abrir Ventana</a>-->
-				<section id=\"openmodal".$id."\" class=\"modalDialog\">
-				<section class=\"modal\">
-				<a href=\"#close\" class=\"close\"> X </a>
-			   	 <section class='modal-item uno'>
-					  	<div class=\"titulo\">
-					   		<h2>".$line[$nombreMascota]."</h2>
-						</div>
-						<div class=\"ubicacion\">
-							<p><img src=\"ubicación.png\">".$line[$direccion]."</p>
-						</div>
-						<div class=\"infoAnimal\">
-					    	<p>".$line[$edad]." años · ".$line[$sexo]." · ".$line[$tamaño]."</p>
-					    </div>
-					    <div class=\"linkProtectora\">
-					    	<a href=\"protectora.php\"><img src=\"caseta.png\">".$line[$nombre]."</a>
-						</div>
-					    <br>
-					    <div class=\"contenidoPerfil\">
-					    	<p>".$line[$descripcion]." </p>
-						</div>
-					    <div class=\"buttons\">
-						   <form>
-						   	 	<input type=\"button\" onclick=\"window.location.href='mailto:protectora@example.com?Subject=Urgente';\" value=\"Contacta con el refugio\" class=\"button\" />
-						   	 	<input type=\"button\" onclick=\"window.location.href='mailto:protectora@example.com?Subject=".$line[$nombreMascota]."';\" value=\"Apadrina a ".$line[$nombreMascota]."\" class=\"button\" />
-							    <button class=\"button\">Compartir</button>
-							 </form>
-						</div>
-					  </section>
-					  <section class=\"imagen\">
-					  	<div class=\"slideshow-container\">
-
-							<div class=\"mySlides fade\">
-							  <div class=\"numbertext\">1 / 3</div>
-							  <img src=".$line[$imagenPerfil]." style=\"width:100%\">
-
+						   		<h2>".$line[$nombreMascota]."</h2>
 							</div>
 							<div class=\"ubicacion\">
-								<p><img src=\"\images\ubicación.png\">".$line[$direccion]."</p>
+								<p><img src=\"ubicación.png\">".$line[$direccion]."</p>
 							</div>
 							<div class=\"infoAnimal\">
 						    	<p>".$line[$edad]." años · ".$line[$sexo]." · ".$line[$tamaño]."</p>
 						    </div>
 						    <div class=\"linkProtectora\">
-						    	<a href=\"protectora.php\"><img src=\"\images\caseta.png\">".$line[$nombre]."</a>
+						    	<a href=\"protectora.php\"><img src=\"caseta.png\">".$line[$nombre]."</a>
 							</div>
 						    <br>
 						    <div class=\"contenidoPerfil\">
@@ -179,38 +140,6 @@
 						</section>
 						</section>
 					</section> 
-							<script>
-                                var slideIndex = 1;
-                                showSlides(slideIndex);
-
-                                function plusSlides(n) {
-                                  showSlides(slideIndex += n);
-                                }
-
-                                function currentSlide(n) {
-                                  showSlides(slideIndex = n);
-                                }
-
-                                function showSlides(n) {
-                                  var i;
-                                  var slides = document.getElementsByClassName('mySlides');
-                                  var dots = document.getElementsByClassName('dot');
-                                  if (n > slides.length) {slideIndex = 1}
-                                  if (n < 1) {slideIndex = slides.length}
-                                  for (i = 0; i < slides.length; i++) {
-                                      slides[i].style.display = 'none';
-                                  }
-                                  for (i = 0; i < dots.length; i++) {
-                                      dots[i].className = dots[i].className.replace('active', '' );
-                                  }
-                                  slides[slideIndex-1].style.display = \"block\";
-                                  dots[slideIndex-1].className += \" active\";
-                                }
-                                </script>
-					 		
-					 	
-					</section>
-					</section>
 				</div>
 			";
 			    
@@ -222,6 +151,5 @@
 		}
 
 		$conn->close();
-			}
-			
 	?>
+
