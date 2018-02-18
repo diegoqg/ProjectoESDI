62946e692241c2de0f939de6fa51d6ed908efe44
paubarbarroja <pau29jastermes@hotmail.com>
2018-02-18 12:01:26 +0100

    asfd

---
+++ pau/bandeja de entrada/BandejaDeEntrada.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="es">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="cssPau.css" title="Color" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<body>
+	
+	<script type="text/javascript">
+		var nombrePerroAnterior;
+		function verMensaje(perro){
+			if(nombrePerroAnterior!=perro.id){
+				nombrePerroAnterior=perro.id;
+				document.getElementById('PBinformacion').innerHTML='';
+				document.getElementById('PBventana').innerHTML='';
+				$.post("ventana.php",{"nombrePerro":perro.id},function(respuesta){
+					$('.PBmensajeVentana').append(respuesta);
+				});
+				
+				
+				$.post("infoPerro.php",{"nombrePerro":perro.id},function(respuesta){
+					$('.PBimagenPerroGeneral').append(respuesta);
+				});
+				window.scrollTo(100,document.body.contenido.PBmensajeVentana.scrollHeight);
+			}
+		}
+		function enviar(vis){
+			mensaje=document.getElementById('PBtextoEnviar').value;
+			
+			$.post("enviar.php",{"nombrePerro":nombrePerroAnterior,"idvis":vis.id,"mensaje":mensaje},function(respuesta){
+				alert(respuesta);
+				document.getElementById('PBinformacion').innerHTML='';
+				$.post("ventana.php",{"nombrePerro":nombrePerroAnterior},function(respuesta){
+					$('.PBmensajeVentana').append(respuesta);
+				});
+			});
+			
+			
+			/*
+			var fecha = new Date();
+			var dia = fecha.getDate();
+			var mes = fecha.getMonth();
+			var año = fecha.getFullYear();
+			
+			if(dia<10){
+				dia='0'+dia
+			}
+			
+			var txt = document.getElementById('PBtextoEnviar').value;
+			
+			var meses = ["ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic"];
+			if(txt==""){
+			}else{
+				$('.PBmensajes').append('<div class="PBmensajeLocal"><p class="PBmensajeTexto">'+txt+'</p><p class="PBmensajeFecha"> '+dia+' de '+meses[mes]+'. de '+año+'</p></div>');
+				$('.PBmensajes').append('<img class="PBimagenLocal" src="imagenes/persona1.jpg" alt="fotoPersona" height="55" width="55">');
+				
+				actualizarDiv();
+				$('.PBtextoEnviar').val('');
+			}*/
+		}
+		<!--Scroll del panell de missatges-->
+		function actualizarDiv(){
+			$('.PBmensajes').animatescroll({ element:'.PBmensajes', padding:20});
+		}
+	</script>
+	
+	
+		<!-- panell dels diferents missatges -->
+		<div class="contenido">
+			<?php
+				include './ventana.php';
+				include './infoPerro.php';
+				include './enviar.php';
+			
+				$servername = "localhost";
+				$username = "root";
+				$password = "1234";
+				$dbname = "pethome";
+
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				
+				$conn->set_charset("utf8");
+				
+				
+				$sql = "SELECT * FROM mensajeria GROUP BY idMascota";
+				$result = mysqli_query($conn, $sql);
+				$sqlcontador = "SELECT DISTINCT idMascota FROM mensajeria";
+				$resultcontador = mysqli_query($conn, $sqlcontador);
+				$contSQL = $resultcontador->num_rows;
+				
+				
+				$idUsuario = "idUsuario";
+				$idProtectora = "idProtectora";
+				$idMascota = "idMascota";
+				$idMensaje = "idMensaje";
+				$mensaje = "mensaje";
+				$fecha = "fecha";
+				$nombre = "nombre";
+				$imagenPerro = "imagenPerfil";
+				$imagen = "imagen";
+				$raza = "raza";
+				$idraza = "idRaza";				
+				echo "<div class=PBbandejaMensajesFuera>";
+				if ($contSQL > 0) {
+					while ($line = mysqli_fetch_array($result)) {
+							$sql2 = "SELECT * FROM mascota";
+							$result2 = mysqli_query($conn, $sql2);
+							
+							while($line2 = mysqli_fetch_array($result2)){
+								if($line2[$idMascota]==$line[$idMascota]){
+									echo "<img class=PBimagenMensajeFuera src=".$line2[$imagenPerro]." alt=fotoPerro height=55 width=55 >";
+							
+									$sql3 = "SELECT * FROM raza";
+									$result3 = mysqli_query($conn, $sql3);
+									
+									while($line3 = mysqli_fetch_array($result3)){
+										if($line2[$raza]==$line3[$idraza]){
+											$sql4 = "SELECT mensaje FROM mensajeria WHERE fecha=(SELECT MAX(fecha) FROM mensajeria WHERE idMascota='".$line2[$idMascota]."')";
+											$result4 = mysqli_query($conn, $sql4);
+											
+											
+											while($line4 = mysqli_fetch_array($result4)){
+													echo "	<div id=".$line2[$nombre]." class=PBmensajeFueraDIV onclick=verMensaje(this)>
+																<p class=PBmensajeNombrePerroFuera> ".$line2[$nombre]."</p>
+																<p class=PBmensajeRazaPerroFuera>".$line3[$nombre]."</p>
+																<p class=PBmensajeFuera>".getSubString($line4[$mensaje],null)."</p>
+															</div>";
+											}
+										}
+									}
+								}
+							}
+						if($contSQL!=1){
+							echo"<hr>";
+						}
+						$contSQL--;
+					}
+				}else{
+					echo "<div class=PBmensajeFueraDIV>
+					<p class=PBmensajeNombrePerroFuera></p>
+					<p class=PBmensajeRazaPerroFuera>No hay mensajes</p>
+					</div>";
+				}
+				echo "</div>";
+				
+				
+				$sql = "SELECT * FROM mensajeria GROUP BY idMascota";
+				$result = mysqli_query($conn, $sql);
+				$sqlcontador = "SELECT DISTINCT idMascota FROM mensajeria";
+				$resultcontador = mysqli_query($conn, $sqlcontador);
+				$contSQL = $resultcontador->num_rows;
+				
+				
+				$conn->close();
+				
+			function getSubString($string, $length=NULL){
+				if ($length == NULL)
+					$length = 23;
+				$stringDisplay = substr(strip_tags($string), 0, $length);
+				if (strlen(strip_tags($string)) > $length)
+					$stringDisplay .= ' ...';
+				return $stringDisplay;
+			}
+			
+			?>
+		
+			<div class="PBmensajeVentana" id="PBventana">
+			</div>
+			<div class="PBimagenPerroGeneral" id="PBinformacion">
+			</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ pau/bandeja de entrada/enviar.php
@@ -0,0 +1,50 @@
+<?php
+	$HolaMundo=1;
+	if(isset($_POST["nombrePerro"],$_POST["idvis"],$_POST["mensaje"])){
+		if($_POST["nombrePerro"]&&$_POST["idvis"]&&$_POST["mensaje"]){
+			session_start();
+			$id = 1;//$_SESSION['id'];
+			$tipo = "usuario"; //$_SESSION['tipo'];
+			$propietario=0;
+			$idMascota=0;
+			
+			if($tipo=="usuario")
+				$propietario=1;
+			if($tipo=="protectora")
+				$propietario=2;
+			
+			
+			$servername = "localhost";
+			$username = "root";
+			$password = "1234";
+			$dbname = "pethome";
+
+			$conn = new mysqli($servername, $username, $password, $dbname);
+								
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+								
+			$conn->set_charset("utf8");
+			
+			$sql = "SELECT * FROM mascota";
+			$result = mysqli_query($conn, $sql);
+			while ($line = mysqli_fetch_array($result)) {
+				if($_POST["nombrePerro"]==$line["nombre"])
+					$idMascota=$line["idMascota"];
+			}
+			
+			$insertsql = "INSERT INTO mensajeria(idUsuario,idProtectora,propietario,idMascota,mensaje,fecha) VALUES (".$id.",".$_POST["idvis"].",".$propietario.",".$idMascota.",'".$_POST["mensaje"]."',SYSDATE())";
+			
+			if (mysqli_query($conn, $insertsql)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $insertsql . "<br>" . mysqli_error($conn);
+			}
+			$conn->close();
+			
+		}
+		
+	}
+		
+?>
