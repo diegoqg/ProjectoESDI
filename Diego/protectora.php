f25d18582c81bea6495267b2745296a0b59b14da
aavell97 <albert.avellaneda12@gmail.com>
2018-02-18 22:19:54 +0100

    Update protectora.php

--- Diego/protectora.php
+++ Diego/protectora.php
@@ -106,8 +106,6 @@
 
     </div>
     <!-- /.container -->
-	<script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- Footer -->
 	<!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
