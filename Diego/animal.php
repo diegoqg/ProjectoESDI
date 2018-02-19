ca04a6f44d3941d9517e0a8c37d0d48087d26c73
Franklinortuno <franklin.ortu@gmail.com>
2018-02-19 11:18:39 +0100

    xd

--- Diego/animal.php
+++ Diego/animal.php
@@ -72,68 +72,34 @@
 								 </form>
 							</div>
 						  </section>
-						  <section class=\"imagen\">
-						  	<div class=\"slideshow-container\">
+						  <section class=\"imagen\" >
+						  	  <div id=\"carouselExampleIndicators\" class=\"carousel slide\" data-ride=\"carousel\">
+						      <ol class=\"carousel-indicators\">
+						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"0\" class=\"active\"></li>
+						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"1\"></li>
+						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"2\"></li>
+						      </ol>
+						      <div class=\"carousel-inner\" role=\"listbox\" style=width: 100%; height: 45%;>
+						        <div class=\"carousel-item active\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagenPerfil]." alt=\"First slide\">
+						        </div>
+						        <div class=\"carousel-item\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagen1]." alt=\"Second slide\">
+						        </div>
+						        <div class=\"carousel-item\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagen2]." alt=\"Third slide\">
+						        </div>
+						      </div>
+						      <a class=\"carousel-control-prev\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"prev\">
+						        <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>
+						        <span class=\"sr-only\">Previous</span>
+						      </a>
+						      <a class=\"carousel-control-next\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"next\">
+						        <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>
+						        <span class=\"sr-only\">Next</span>
+						      </a>
+						    </div>
 
-								<div class=\"mySlides fade\">
-								  <div class=\"numbertext\">1 / 3</div>
-								  <img src=".$line[$imagenPerfil]." style=\"width:100%\">
-								
-								</div>
-
-								<div class=\"mySlides fade\">
-								  <div class=\"numbertext\">2 / 3</div>
-								  <img src=".$line[$imagen1]." style=\"width:100%\">
-								
-								</div>
-
-								<div class=\"mySlides fade\">
-								  <div class=\"numbertext\">3 / 3</div>
-								  <img src=".$line[$imagen2]." style=\"width:100%\">
-								 
-								</div>
-
-								<a class=\"prev\" onclick=\"plusSlides(-1)\">&#10094;</a>
-								<a class=\"next\" onclick=\"plusSlides(1)\">&#10095;</a>
-
-								</div>
-								<br>
-
-								<div style=\"text-align:center\">
-								  <span class=\"dot\" onclick=currentSlide(1)></span> 
-								  <span class=\"dot\" onclick=currentSlide(2)></span> 
-								  <span class=\"dot\" onclick=currentSlide(3)></span> 
-								</div>
-							</section>	
-
-								<script>
-								var slideIndex = 1;
-								showSlides(slideIndex);
-
-								function plusSlides(n) {
-								  showSlides(slideIndex += n);
-								}
-
-								function currentSlide(n) {
-								  showSlides(slideIndex = n);
-								}
-
-								function showSlides(n) {
-								  var i;
-								  var slides = document.getElementsByClassName('mySlides');
-								  var dots = document.getElementsByClassName('dot');
-								  if (n > slides.length) {slideIndex = 1}    
-								  if (n < 1) {slideIndex = slides.length}
-								  for (i = 0; i < slides.length; i++) {
-								      slides[i].style.display = 'none';  
-								  }
-								  for (i = 0; i < dots.length; i++) {
-								      dots[i].className = dots[i].className.replace('active', '' );
-								  }
-								  slides[slideIndex-1].style.display = \"block\";  
-								  dots[slideIndex-1].className += \" active\";
-								}
-								</script>
 						 		
 						 	
 							</section>
