a8ad050f529f52f14adf45c0fcbe8fb4da9229e1
Franklinortuno <franklin.ortu@gmail.com>
2018-02-19 11:51:45 +0100

    xd

--- Diego/animal.php
+++ Diego/animal.php
@@ -81,13 +81,13 @@
 						      </ol>
 						      <div class=\"carousel-inner\" role=\"listbox\" style=\"width: 100%; height: 45%;\">
 						        <div class=\"carousel-item active\">
-						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby.jpg\" alt=\"First slide\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagenPerfil]." alt=\"First slide\">
 						        </div>
 						        <div class=\"carousel-item\">
-						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby2.jpg\" alt=\"Second slide\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagen1]." alt=\"Second slide\">
 						        </div>
 						        <div class=\"carousel-item\">
-						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby3.jpg\" alt=\"Third slide\">
+						          <img class=\"d-block img-fluid\" src=".$line[$imagen2]." alt=\"Third slide\">
 						        </div>
 						      </div>
 						      <a class=\"carousel-control-prev\" href=\"#carousel".$id."\" role=\"button\" data-slide=\"prev\">
