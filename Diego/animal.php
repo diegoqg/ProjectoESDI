5ddf51f67cb70a7c14ae361a735bcf57e7442152
diegoqg <diegoqueraltgalan@gmail.com>
2018-02-19 11:37:13 +0100

    a
    a

--- Diego/animal.php
+++ Diego/animal.php
@@ -73,28 +73,28 @@
 							</div>
 						  </section>
 						  <section class=\"imagen\" >
-						  	  <div id=\"carouselExampleIndicators\" class=\"carousel slide\" data-ride=\"carousel\">
+						  	  <div id=\"carousel".$id."\" class=\"carousel slide\" data-ride=\"carousel\">
 						      <ol class=\"carousel-indicators\">
-						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"0\" class=\"active\"></li>
-						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"1\"></li>
-						        <li data-target=\"#carouselExampleIndicators\" data-slide-to=\"2\"></li>
+						        <li data-target=\"#carousel".$id."\" data-slide-to=\"0\" class=\"active\"></li>
+						        <li data-target=\"#carousel".$id."\" data-slide-to=\"1\"></li>
+						        <li data-target=\"#carousel".$id."\" data-slide-to=\"2\"></li>
 						      </ol>
-						      <div class=\"carousel-inner\" role=\"listbox\" style=width: 100%; height: 45%;>
+						      <div class=\"carousel-inner\" role=\"listbox\" style=\"width: 100%; height: 45%;\">
 						        <div class=\"carousel-item active\">
-						          <img class=\"d-block img-fluid\" src=".$line[$imagenPerfil]." alt=\"First slide\">
+						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby.jpg\" alt=\"First slide\">
 						        </div>
 						        <div class=\"carousel-item\">
-						          <img class=\"d-block img-fluid\" src=".$line[$imagen1]." alt=\"Second slide\">
+						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby2.jpg\" alt=\"Second slide\">
 						        </div>
 						        <div class=\"carousel-item\">
-						          <img class=\"d-block img-fluid\" src=".$line[$imagen2]." alt=\"Third slide\">
+						          <img class=\"d-block img-fluid\" src=\"images/HistÃ²ria toby3.jpg\" alt=\"Third slide\">
 						        </div>
 						      </div>
-						      <a class=\"carousel-control-prev\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"prev\">
+						      <a class=\"carousel-control-prev\" href=\"#carousel".$id."\" role=\"button\" data-slide=\"prev\">
 						        <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>
 						        <span class=\"sr-only\">Previous</span>
 						      </a>
-						      <a class=\"carousel-control-next\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"next\">
+						      <a class=\"carousel-control-next\" href=\"#carousel".$id."\" role=\"button\" data-slide=\"next\">
 						        <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>
 						        <span class=\"sr-only\">Next</span>
 						      </a>
